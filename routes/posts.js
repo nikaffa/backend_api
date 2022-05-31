7ed5f75685df6b414f49432c8bbb1bef40db7657
Veronika Fatikhova <fidelio@list.ru>
2022-05-31 15:13:59 -0700

    axios get works with hardcoded api

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,19 @@
 //All routes for Posts are defined here
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 const postsRoutes = (app) => {
 
   // GET all posts
-  app.get('/', (req, res) => {
-    res.status(200).send({ "posts":true });
+  app.get('/:tags/:sortBy?/:direction?', (req, res) => {
+    axios.get(`https://app.hatchways.io/api/assessment/blog/posts?tag=tech`)
+      .then(data => {
+        res.status(200).send({ 'posts': data.data.posts });
+      })
+      .catch(err => {
+        console.error('error', err.message);
+      });
   });
 
   return router;
